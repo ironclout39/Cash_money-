8b2f869fb3566a8d3fa9869f0795ba50e672dc0b
SlnDev <73367319+SlnDev@users.noreply.github.com>
2020-10-24 17:57:01 -0400

    Revert "Scripts"
    This reverts commit 33670878bcbadfbcd9624891d31c0a794dda44fa.

--- Cash_money/Assets/NewBehaviourScript.cs
+++ Cash_money/Assets/NewBehaviourScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Read_This : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class Read_This : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        print("Hello Nigga");
+        
     }
 }
